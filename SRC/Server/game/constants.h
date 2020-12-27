6a7ae121600b65e58ca8a4ef9419c4e9284ddb8a
Robert <55710848+Rakancito@users.noreply.github.com>
2020-12-27 15:40:20 -0600

    Create constants.h

---
+++ SRC/Server/game/constants.h
@@ -0,0 +1,9 @@
+//Search
+
+extern const DWORD		party_exp_distribute_table[PLAYER_EXP_TABLE_M
+
+//add after
+
+#ifdef ENABLE_CONQUEROR_LEVEL
+extern const DWORD		exp_coqueror_table[PLAYER_MAX_CONQUEROR_LEVEL_CONST + 1];
+#endif
